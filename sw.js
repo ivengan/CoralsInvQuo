635f487adf64e62a054d69a72d8a2d9d9523a916
ivengan <46960011+ivengan@users.noreply.github.com>
2025-11-23 17:17:58 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "invoice-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js",
+  "https://cdn.sheetjs.com/xlsx-latest/package/dist/xlsx.full.min.js",
+  "https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js"
+];
+
+// Install Event
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Serve from Cache)
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
